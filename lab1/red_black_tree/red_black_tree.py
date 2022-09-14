7e51f66afcff797746cb21299673e265d0979342
Nicolas <93177243+MykolaKovalyk@users.noreply.github.com>
2022-09-14 07:18:42 +0300

    Working red-black tree insertion

---
+++ lab1/red_black_tree/red_black_tree.py
@@ -0,0 +1,333 @@
+from __future__ import annotations
+from typing import Any, List, Tuple, Callable
+
+
+RED = True
+BLACK =  False
+NULL = None
+
+class RedBlackTree:
+
+    class Node:
+
+        def __init__(self, key: Any):
+
+            self.key = key
+            self.value = None
+
+            self.left = None
+            self.right = None
+            self.parent = None
+
+            self.color = RED
+
+        def set_left(self, new_left_node: RedBlackTree.Node) -> RedBlackTree.Node:
+            old_node = self.left
+            if self.left is not NULL:
+                self.left.parent = NULL
+
+            self.left = new_left_node
+
+            if self.left is not NULL:
+                self.left.parent = self
+
+            return old_node
+
+        def set_right(self, new_right_node: RedBlackTree.Node) -> RedBlackTree.Node:
+            old_node = self.right
+            if self.right is not NULL:
+                self.right.parent = NULL
+
+            self.right = new_right_node
+
+            if self.right is not NULL:
+                self.right.parent = self
+
+            return old_node
+
+        def is_right(self):
+            if self.parent is NULL:
+                return None
+
+            return self is self.parent.right
+
+    def __init__(self):
+
+        global NULL
+
+        if NULL is None:
+            NULL = RedBlackTree.Node(None)
+            NULL.color = BLACK
+
+        self.root = NULL
+        self._size = 0
+
+    def __getitem__(self, key):
+
+        found_node = self._find_node_by_key(key)
+        return found_node.value if found_node is not NULL else None
+
+    def __setitem__(self, key: Any, value: Any):
+        found_node = self._add_or_find_node_by_key(key, new_only=False)
+        found_node.value = value
+
+    def __str__(self):
+
+        final_string = ""
+
+        for item in self._to_list_of_nodes():
+
+            current_parent = item.parent
+            identation = 0
+            while current_parent is not NULL:
+                identation += 1
+                current_parent = current_parent.parent
+
+            final_string += f"\n{'-' * identation} {'r' if item.color else 'b'} {'r' if item.is_right() else 'l'} [{hash(item.key)}] {item.key}"
+
+        return final_string
+
+    def insert(self, key: Any, value: Any):
+        node = self._find_node_by_key(key)
+
+        if node is NULL:
+            raise ValueError(f'Entry with the key "{key}" was not found.')
+        
+        node.value = value
+    
+    def append(self, key: Any, value: Any):
+        node = self._add_or_find_node_by_key(key, new_only=True)
+        node.value = value
+
+    def remove(self, key: Any) -> Any:
+
+        found_node = self._find_node_by_key(key)
+
+        if found_node is NULL:
+            return None
+
+        value = found_node.value
+        self._remove_node(found_node)
+        return value
+
+    def remove_by(self, predicate: Callable[[Any, Any], bool]) -> List[(Any, Any)]:
+
+        list_to_remove = self.get_by(predicate)
+
+        for key, item in list_to_remove:
+            self.remove(key)
+
+        return list_to_remove
+
+    def get_by(self, predicate: Callable[[Any, Any], bool]) -> List[(Any, Any)]:
+        return [(x.key, x.value) for x in self._to_list_of_nodes() if predicate(x.key, x.value)]
+
+    def top_to_bottom_list(self) -> List[(Any, Any)]:
+        return [(x.key, x.value) for x in self._to_list_of_nodes()]
+
+    def size(self) -> int:
+        return self._size
+
+    def _to_list_of_nodes(self) -> List[RedBlackTree.Node]:
+        result = []
+
+        def save_children(node: RedBlackTree.Node):
+            if node.left is not NULL:
+                result.append(node.left)
+                save_children(node.left)
+            if node.right is not NULL:
+                result.append(node.right)
+                save_children(node.right)
+
+        if self.root is NULL:
+            return result
+
+        result.append(self.root)
+        save_children(self.root)
+
+        return result
+
+    def _find_node_by_key(self, key: Any) -> RedBlackTree.Node:
+
+        current_node = self.root
+
+        while current_node is not NULL and key != current_node.key:
+            right_node = hash(key) > hash(current_node.key)
+            current_node = current_node.right if right_node else current_node.left
+
+        return current_node
+
+    def _add_or_find_node_by_key(self, key: Any, new_only: bool) -> RedBlackTree.Node:
+
+        current_node = self.root
+        node_parent = NULL
+        right_node = False
+        
+        while current_node is not NULL and key != current_node.key:
+            node_parent = current_node
+            right_node = hash(key) > hash(current_node.key)
+            current_node = current_node.right if right_node else current_node.left
+
+        if current_node is not NULL:
+            if new_only:
+                raise ValueError(f'Entry with the key "{key}" already exists.')
+        if current_node is NULL:
+            current_node = self._new_node(key)
+
+            if self.root is NULL:
+                self.root = current_node
+            elif right_node:
+                node_parent.set_right(current_node)
+            else:
+                node_parent.set_left(current_node)
+            
+            self._size += 1
+            self._insert_fixup(current_node)
+
+        return current_node
+
+    def _new_node(self, key):
+        new_node = RedBlackTree.Node(key)
+        new_node.left = new_node.right = new_node.parent = NULL
+
+        return new_node
+
+    def _replace_node(self, node_to_replace: RedBlackTree.Node) -> RedBlackTree.Node:
+
+        replacement = node_to_replace
+
+        if node_to_replace.right is not NULL:
+            replacement = self._find_the_smallest_node_in_the_branch(node_to_replace.right)
+        elif node_to_replace.left is not NULL:
+            replacement = self._find_the_biggest_node_in_the_branch(node_to_replace.left)
+
+        node_to_replace.key = replacement.key
+        node_to_replace.value = replacement.value
+
+        return replacement
+
+    def _prune_leaf_node(self, node_to_prune: RedBlackTree.Node) -> None:
+        
+        if node_to_prune.parent is NULL:
+            self._root = NULL
+        else:
+            replacement = node_to_prune.left if node_to_prune.left is not NULL else node_to_prune.right
+            if node_to_prune.is_right():
+                node_to_prune.parent.set_right(replacement)
+            else:
+                node_to_prune.parent.set_left(replacement)
+        
+        self._size -= 1
+
+    def _remove_node(self, node_to_remove: RedBlackTree.Node) -> RedBlackTree.Node:
+        prune_node =  self._replace_node(node_to_remove)
+        self._remove_fixup(prune_node)
+        self._prune_leaf_node(prune_node)
+
+    def get_max_depth(self):
+        depth = 0
+        for node in self._to_list_of_nodes():
+            current_depth = 1
+            while node.parent is not NULL:
+                node =  node.parent
+                current_depth += 1
+            
+            if current_depth > depth:
+                depth =  current_depth
+
+        return depth
+
+    def _insert_fixup(self, node): 
+
+        if node.parent is NULL:
+            node.color = BLACK
+            return
+
+        if node.parent.parent is NULL:
+            return
+
+        while node.parent.color is RED:
+            if node.parent == node.parent.parent.right:
+                uncle = node.parent.parent.left
+                if uncle.color is RED:
+                    uncle.color = BLACK
+                    node.parent.color = BLACK
+                    node.parent.parent.color = RED
+                    node = node.parent.parent
+                else:
+                    if node is node.parent.left:
+                        node = node.parent
+                        self._right_rotate(node)
+                    node.parent.color = BLACK
+                    node.parent.parent.color = RED
+                    self._left_rotate(node.parent.parent)
+            else:
+                uncle = node.parent.parent.right
+
+                if uncle.color is RED:
+                    uncle.color = BLACK
+                    node.parent.color = BLACK
+                    node.parent.parent.color = RED
+                    node = node.parent.parent
+                else:
+                    if node is node.parent.right:
+                        node = node.parent
+                        self._left_rotate(node)
+                    node.parent.color = BLACK
+                    node.parent.parent.color = RED
+                    self._right_rotate(node.parent.parent)
+            if node is self.root:
+                break
+
+        self.root.color = BLACK
+
+    def _remove_fixup(self, node):
+        pass
+
+    def _left_rotate(self, node):
+        right_node = node.right
+        node.right = right_node.left
+        if right_node.left != NULL:
+            right_node.left.parent = node
+
+        right_node.parent = node.parent
+        if node.parent == NULL:
+            self.root = right_node
+        elif node == node.parent.left:
+            node.parent.left = right_node
+        else:
+            node.parent.right = right_node
+        right_node.left = node
+        node.parent = right_node
+
+    def _right_rotate(self, node):
+        left_node = node.left
+        node.left = left_node.right
+        if left_node.right != NULL:
+            left_node.right.parent = node
+
+        left_node.parent = node.parent
+        if node.parent == NULL:
+            self.root = left_node
+        elif node == node.parent.right:
+            node.parent.right = left_node
+        else:
+            node.parent.left = left_node
+        left_node.right = node
+        node.parent = left_node
+
+    def _find_the_smallest_node_in_the_branch(self, node: RedBlackTree.Node) -> RedBlackTree.Node:
+
+        current_node = node
+        while current_node.left is not NULL:
+            current_node = current_node.left
+
+        return current_node
+
+    def _find_the_biggest_node_in_the_branch(self, node: RedBlackTree.Node) -> RedBlackTree.Node:
+
+        current_node = node
+        while current_node.right is not NULL:
+            current_node = current_node.right
+
+        return current_node
---
+++ lab1/red_black_tree/red_black_tree_test.py
@@ -0,0 +1,132 @@
+import unittest
+import random
+from red_black_tree import RedBlackTree
+from math import log2
+
+
+class Film:
+
+    def __init__(self, name, year, duration_in_minutes, genre, studio):
+        self.name = name
+        self.year = year
+        self.duration_in_minutes = duration_in_minutes
+        self.genre = genre
+        self.studio = studio
+
+
+    def __str__(self):
+        return  f'---------- film ---------- \n'\
+                f'name: {self.name} \n' \
+                f'year: {self.year} \n ' \
+                f'genre: {self.genre} \n' \
+                f'studio: {self.studio} \n' \
+                f'duration: {self.duration_in_minutes}'
+
+class MyTestCase(unittest.TestCase):
+
+    def test_binary_tree(self):
+
+        def run_test():
+            names = [
+                'Top gun',
+                'Avatar',
+                'Illovaysk',
+                'Inception',
+                'Pacific rim',
+                'Transformers',
+                'Interstellar',
+                'Oblivion',
+                'Dune',
+                'Zahar Berkut',
+                'World War Z',
+                'Fury',
+                'Harry Potter and the Sorcerer\'s Stone',
+                'The Angry Birds Movie',
+                'Valkyrie',
+                'American Made',
+                'Joker',
+                'Deadpool'
+            ]
+
+            random.shuffle(names)
+
+            test_list = []
+            for i in range(len(names)):
+                test_list.append(Film(
+                    names[i],
+                    random.randint(2000, 2022),
+                    random.randint(120, 180),
+                    'action',
+                    'Hollywood'
+                ))
+
+            bin_tree = RedBlackTree()
+            self.assertEqual(bin_tree.size(), 0)
+
+            for film in test_list:
+                bin_tree.append(film.name, film)
+
+            self.assertEqual(bin_tree.size(), len(test_list))
+            
+            '''
+            test_subject_1 = test_list[random.randint(0, len(test_list) - 1)]
+            test_list.remove(test_subject_1)
+            bin_tree.remove(test_subject_1.name)
+            self.assertEqual(bin_tree.size(), len(test_list))
+
+            test_subject_2 = test_list[random.randint(0, len(test_list) - 1)]
+            test_list.remove(test_subject_2)
+            bin_tree.remove(test_subject_2.name)
+            self.assertEqual(bin_tree.size(), len(test_list))
+
+            bin_tree.append(test_subject_1.name, test_subject_1)
+            bin_tree.append(test_subject_2.name, test_subject_2)
+
+            self.assertEqual(bin_tree[test_subject_1.name], test_subject_1)
+            self.assertEqual(bin_tree[test_subject_2.name], test_subject_2)
+
+            bin_tree.insert(test_subject_1.name, test_subject_2)
+            bin_tree[test_subject_2.name] = test_subject_1
+
+            self.assertEqual(bin_tree[test_subject_1.name], test_subject_2)
+            self.assertEqual(bin_tree[test_subject_2.name], test_subject_1)
+
+            removed = [y for (x, y) in bin_tree.remove_by(
+                lambda key, value: key is test_subject_1.name or key is test_subject_2.name)]
+            self.assertIn(test_subject_1, removed)
+            self.assertIn(test_subject_2, removed)
+
+            self.assertEqual(bin_tree[test_subject_1.name], None)
+            for film in test_list:
+                self.assertEqual(bin_tree[film.name], film)
+
+            self.assertEqual(bin_tree[test_subject_2.name], None)
+            for film in test_list:
+                self.assertEqual(bin_tree[film.name], film)
+
+            remove_test_list = list(test_list)
+            while len(remove_test_list) > 0:
+                i = len(remove_test_list) - 1
+                to_remove = remove_test_list[i]
+                remove_test_list.remove(to_remove)
+                bin_tree.remove(to_remove.name)
+
+                for film in remove_test_list:
+                    self.assertEqual(bin_tree[film.name], film)
+
+            self.assertEqual(bin_tree.size(), 0, bin_tree)
+            try:
+                for film in test_list:
+                    bin_tree.append(film.name, film)
+            except Exception as e:
+                self.assertFalse(True, f"{e}, {bin_tree.size()} {bin_tree}")
+            '''
+
+            self.assertLessEqual(bin_tree.get_max_depth(),  int(2 * log2(bin_tree.size())))
+
+        for i in range(1000):
+            run_test()
+
+
+if __name__ == '__main__':
+    unittest.main()
