e6515ae15cf72bfdd4b9272ebacf1e3c6406731d
Nicolas <93177243+MykolaKovalyk@users.noreply.github.com>
2022-10-09 22:09:33 +0300

    Add code for lab 3

--- lab2/bfs.py
+++ lab2/bfs.py
@@ -3,74 +3,100 @@
 import csv
 from typing import Dict, List
 
-def get_min_depth(root, nodes, connections):
-    
+def bfs(root, connections):
+    found_nodes = []
+
+    nodes = []
+    for connection in connections:
+        for node in connection:
+            if node in nodes:
+                continue 
+            nodes.append(node)
     
     marked = {}
-    marked[root] = False
     for node in nodes:
         marked[node] = False
 
     queue = [root]
-    shortest_path_node = None
     while len(queue) > 0:
         current_node = queue.pop(0)
-        
+        found_nodes.append(current_node)
+
         if not marked[current_node]:
             marked[current_node] = True
 
             neighbours = []
             for connection in connections:
-                if connection[0] == current_node:
-                    neighbours.append(connection[1])
-
-            if len(neighbours) == 0:
-                shortest_path_node = current_node
-                break
+                if not current_node in connection:
+                    continue
+                for node in connection:
+                    if node != current_node:
+                        neighbours.append(node)
 
             for neighbour in neighbours:
                 if not marked[neighbour]:
                     queue.append(neighbour)
     
-    depth = 1
-    current_node = shortest_path_node
-    
-    found_parent = True
-    while found_parent:
-        found_parent = False
-        for connection in connections:
-            if connection[1] == current_node:
-                current_node = connection[0]
-                depth+=1
-                found_parent = True
-                break
-    
-    return depth
+    return found_nodes
 
+
+def get_pairs_of_people(tribes):
+    pairs_of_people = []
+
+    for tribe in tribes:
+        men = [member for member in tribe if member % 2 == 1]
+        women = [member for member in tribe if member % 2 == 0]
+        
+        for another_tribe in tribes:
+            if another_tribe == tribe:
+                continue
+
+            another_men = [member for member in another_tribe if member % 2 == 1]
+            another_women = [member for member in another_tribe if member % 2 == 0]
+
+            for man in men:
+                for woman in another_women:
+                    potential_pair = [man, woman]
+                    if not potential_pair in pairs_of_people:
+                        pairs_of_people.append(potential_pair)
+
+            for woman in women:
+                for man in another_men:
+                    potential_pair = [man, woman]
+                    if not potential_pair in pairs_of_people:
+                        pairs_of_people.append(potential_pair)
+
+    return pairs_of_people
 
 
 
 if __name__ == "__main__":
     connections = []
-    with open("input.txt", "r") as input_file:
-        csv_reader = csv.reader(input_file, delimiter=',')
-        for csv_row in csv_reader:
-            csv_row[0] = int(csv_row[0])
+    count_of_pairs = int(input("Enter number of pairs: "))
+    print("Enter the pairs: ")
+    for i in range(count_of_pairs):
+        person, another = input(" > ").split()
+        connections.append([int(person), int(another)])
 
-            if len(csv_row) > 1:
-                csv_row[1] = int(csv_row[1])
-            
-            connections.append(csv_row)
-    
-    nodes = []
+
     root = connections[0][0]
-    del connections[0]
 
+    nodes = []
     for connection in connections:
-        nodes.append(connection[-1])
+        for node in connection:
+            if node in nodes:
+                continue 
+            nodes.append(node)
 
-    shortest_root_null_path = get_min_depth(root, nodes, connections)
-    print(f"Shortest path is: {shortest_root_null_path}")
+    tribes = [] 
+    while len(nodes) > 0:
+        tribe = bfs(nodes[0], connections)
+        for node in tribe:
+            nodes.remove(node)
+        tribes.append(tribe)
+    
+    pairs = get_pairs_of_people(tribes)
 
-    with open("output.txt", "w+") as input_file:
-        input_file.write(str(shortest_root_null_path))
+    print(f"Tribes: {tribes}")
+    print(f"Count of people: {len(pairs)}")
+    print(f"Pairs: {pairs}")
